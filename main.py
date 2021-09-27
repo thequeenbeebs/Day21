aa80512ca802a4063c2a48e90d39c8b81ae4048f
Blaire Baker <bakerblaire@gmail.com>
2021-09-27 16:48:39 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# INHERITANCE
+
+# CLASS INHERITANCE
+#   Inheriting behavior and appearance from an existing class
+#   class Fish(Animal):
+#       def __init__(self):
+#           super().__init__()
+
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def swim(self):
+        print("moving in water")
+
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater.")
+
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
